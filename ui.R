c2c247a11d3a20b887395cafbd7867bca745e9af
Guilleferchero <guilleferchero@gmail.com>
2023-07-11 21:51:12 -0300

    Se agregó menu item en el sidebar

--- ui.R
+++ ui.R
@@ -7,14 +7,21 @@ dashboardPage(
   dashboardSidebar(
     textInput("usuario","Cual es tu usuario?", "guillebarracas"),
     actionButton("update","buscar"),
-    textOutput("text")
+    textOutput("text"),
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard"),
+      menuItem("Raw data", tabName = "rawdata")
+    )
   ),
   dashboardBody(
-    fluidRow(
-      valueBoxOutput("partidas", width = 4),
-      valueBoxOutput("fechaini", width = 4),
-      valueBoxOutput("fechafin", width = 4)),
-    fluidRow(
-      box(plotlyOutput("plot1"), width = 4),
-      box(plotlyOutput("plot2"), width = 8)),
-    DTOutput("table")))
+    tabItems(
+      tabItem(tabName = "dashboard",
+        fluidRow(
+          valueBoxOutput("partidas", width = 4),
+          valueBoxOutput("fechaini", width = 4),
+          valueBoxOutput("fechafin", width = 4)),
+        fluidRow(
+          box(plotlyOutput("plot1"), width = 4),
+          box(plotlyOutput("plot2"), width = 8))),
+    tabItem(tabName = "rawdata",
+          DTOutput("table")))))
